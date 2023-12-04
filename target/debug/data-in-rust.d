47b19065c54f155cc2d461f343d0736b5e63017d
Alex Tran <hoangpichgoodkid@gmail.com>
2023-12-04 13:59:16 +0700

    Update app

--- target/debug/data-in-rust.d
+++
@@ -1,1 +0,0 @@
-D:\Web3\RUST\my-project\data-in-rust\target\debug\data-in-rust.exe: D:\Web3\RUST\my-project\data-in-rust\src\main.rs
--- target/debug/deps/data_in_rust-a243b734b6728795.d
+++
@@ -1,5 +0,0 @@
-D:\Web3\RUST\my-project\data-in-rust\target\debug\deps\data_in_rust-a243b734b6728795.exe: src\main.rs
-
-D:\Web3\RUST\my-project\data-in-rust\target\debug\deps\data_in_rust-a243b734b6728795.d: src\main.rs
-
-src\main.rs:
