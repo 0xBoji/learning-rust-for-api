72d32dfe643bc5a6cea8f3ccc1281baa6fb5f9c8
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2023-12-24 23:02:15 +0700

    Updated: server

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,18 @@
-#![allow(dead_code)] // this line prevents compiler warnings
-enum Species {
-    Crab,
-    Octopus,
-    Fish,
-    Clam
-}
+#![allow(unused)]
+use std::net::SocketAddr;
 
-struct SeaCreature {
-    species: Species,
-    name: String,
-    arms: i32,
-    legs: i32,
-    weapon: String,
-}
-
-fn main() {
-    let ferris = SeaCreature {
-        species: Species::Crab,
-        name: String::from("Ferris"),
-        arms: 2,
-        legs: 4,
-        weapon: String::from("claw"),
-    };
-    let octopus = SeaCreature {
-        species: Species::Octopus,
-        name: String::from("Octopus"),
-        arms: 2,
-        legs: 4,
-        weapon: String::from("no thing"),
-    };
-
-
-    match ferris.species {
-        Species::Crab => println!("{} là một con cua",ferris.name),
-        Species::Octopus => println!("{} là một con bạch tuộc",ferris.name),
-        Species::Fish => println!("{} là một con cá",ferris.name),
-        Species::Clam => println!("{} là một con hến",ferris.name),
-    }
-
-    match octopus.species {
-        Species::Crab => println!("{} là một con cua",octopus.name),
-        Species::Octopus => println!("{} là một con bạch tuộc",octopus.name),
-        Species::Fish => println!("{} là một con cá",octopus.name),
-        Species::Clam => println!("{} là một con hến",octopus.name),
-    }
+use axum::Router;
+use axum::routing::get;
+use axum::response::Html;
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async{ Html("HelloWorld") }),
+    );
+    let addr = SocketAddr::from(([127,0,0,1], 8080));
+    axum::Server::bind(&addr)
+    .serve(routes_hello.into_make_service())
+    .await
+    .unwrap();
 }
